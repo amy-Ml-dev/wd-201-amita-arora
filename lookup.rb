15db7058dc21fcdc438d907b13aae0562ea5e238
Amita Arora <77974739+amy-Ml-dev@users.noreply.github.com>
2021-09-25 07:53:29 +0530

    Update lookup.rb

--- lookup.rb
+++ lookup.rb
@@ -7,22 +7,23 @@ def get_command_line_argument
 end
 
 domain = get_command_line_argument
+
 dns_raw = File.readlines("zone")
 
-def parse_dns(original)
-  original = original.reject { |line| line.match(/^$/) }
-  original = original.reject { |line| line.match(/^#/) }
-  mapped = Array.new
-  mapped = original.map { |line| line.strip.split(",") }
-  hash = Hash.new
-  hash = mapped.each_with_object({}) { |record, i| i[record[1].strip] = { type: record[0].strip, destination: record[2].strip } }
-  return hash
+def parse_dns(dns_raw)
+  mapped = dns_raw.
+    reject { |line| line.match(/^#/) }.reject { |line| line.empty? }.
+    map { |line| line.strip.split(", ") }
+
+  return mapped.each_with_object({}) do |i, records|
+           records[i[1]] = { :type => i[0], :destination => i[2] }
+         end
 end
 
 def resolve(dns_records, lookup_chain, domain)
   fetched_record = dns_records.fetch(domain, nil)
   if fetched_record == nil
-    lookup_chain << "Error: record not found for #{domain}"
+    lookup_chain << "No record found for #{domain}"
   elsif fetched_record[:type] == "A"
     lookup_chain << fetched_record[:destination]
   elsif fetched_record[:type] == "CNAME"
@@ -32,6 +33,7 @@ def resolve(dns_records, lookup_chain, domain)
 end
 
 dns_records = parse_dns(dns_raw)
+
 lookup_chain = [domain]
 lookup_chain = resolve(dns_records, lookup_chain, domain)
-puts lookup_chain.join(" => ")
+puts lookup_chain.join("=>")
