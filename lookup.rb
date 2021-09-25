f291a70fb960b8bc97e2c5463359f26a47ce9c17
Amita Arora <77974739+amy-Ml-dev@users.noreply.github.com>
2021-09-25 19:04:10 +0530

    Update lookup.rb

--- lookup.rb
+++ lookup.rb
@@ -7,18 +7,18 @@ def get_command_line_argument
 end
 
 domain = get_command_line_argument
+
 dns_raw = File.readlines("zone")
 
 def parse_dns(dns_raw)
   dns_raw.
-    reject {|line| line.empty? }.
-    map {|line| line.strip.split(", ") }.
-    reject do |record|
-      # 'Reject' records that aren't valid.
-    end.
-    each_with_object({}) do |record, records|
-      # Modify the `records` hash so that it contains necessary details.
+    reject { |line| line.match(/^#/) }.reject { |line| line.empty? }.
+    map { |line| line.strip.split(", ") }.reject do |record|
+      record[0] != "A" && record[0] != "CNAME"
     end
+  .each_with_object({}) do |i, records|
+           records[i[1]] = { :type => i[0], :destination => i[2] }
+         end
 end
 
 def resolve(dns_records, lookup_chain, domain)
@@ -32,6 +32,12 @@ def resolve(dns_records, lookup_chain, domain)
     resolve(dns_records, lookup_chain, fetched_record[:destination])
   end
 end
+
+dns_records = parse_dns(dns_raw)
+
+lookup_chain = [domain]
+lookup_chain = resolve(dns_records, lookup_chain, domain)
+puts lookup_chain.join("=>")
 
 dns_records = parse_dns(dns_raw)
 lookup_chain = [domain]
