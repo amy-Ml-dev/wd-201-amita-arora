695b19523beca6501ee3c3f5ba42ae941bc807a3
Amita Arora <77974739+amy-Ml-dev@users.noreply.github.com>
2021-09-25 18:05:04 +0530

    Update lookup.rb

--- lookup.rb
+++ lookup.rb
@@ -7,17 +7,18 @@ def get_command_line_argument
 end
 
 domain = get_command_line_argument
-
 dns_raw = File.readlines("zone")
 
 def parse_dns(dns_raw)
-  mapped = dns_raw.
-    reject { |line| line.match(/^#/) }.reject { |line| line.empty? }.
-    map { |line| line.strip.split(", ") }
-
-  return mapped.each_with_object({}) do |i, records|
-           records[i[1]] = { :type => i[0], :destination => i[2] }
-         end
+  dns_raw.
+    reject {|line| line.empty? }.
+    map {|line| line.strip.split(", ") }.
+    reject do |record|
+      # 'Reject' records that aren't valid.
+    end.
+    each_with_object({}) do |record, records|
+      # Modify the `records` hash so that it contains necessary details.
+    end
 end
 
 def resolve(dns_records, lookup_chain, domain)
@@ -33,7 +34,7 @@ def resolve(dns_records, lookup_chain, domain)
 end
 
 dns_records = parse_dns(dns_raw)
-
 lookup_chain = [domain]
 lookup_chain = resolve(dns_records, lookup_chain, domain)
 puts lookup_chain.join("=>")
+
